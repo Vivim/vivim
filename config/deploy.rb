e5dc4efc7ff3ed346e361fefdbb7b98618cbaafc
Maksim Baranov <openok1989@mail.ru>
2014-07-10 14:52:22 +0300

    fix for deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,4 +4,4 @@ set :application, 'vivim'
 set :repo_url, 'git@github.com:Vivim/vivim.git'
 set :linked_dirs, fetch(:linked_dirs, []) + %w[bin log tmp/pids tmp/cache tmp/sockets public/system]
 set :linked_files, %w[config/database.yml config/secrets.yml]
-set :log_level, :debug
+# set :log_level, :debug
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,4 +3,5 @@ server '54.200.246.12', user: 'ubuntu', roles: [:app, :web, :db]
 set :stage, :production
 set :rails_env, :production
 set :deploy_to, '/home/ubuntu/vivim'
-set :ssh_options, { forward_agent: true }
+set :ssh_options, { forward_agent: true, auth_methods: ['publickey'], keys: [ "~/.ssh/vivim.pem" ] }
+
