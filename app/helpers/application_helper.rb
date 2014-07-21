2603207ae81e9277d76fb4d46c00638281dbcdd2
Maksim Baranov <openok1989@mail.ru>
2014-07-21 13:44:13 +0300

    changes for SEO

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+	def title_phrase
+		'Denver Mobile App Development | Web Design | UX/UI Design'
+	end
+
+	def meta_description_content
+		'Denver Mobile App Development company focused on making cutting edge apps for iPhone/iPad and Android devices. Denver mobile application developers, Denver Web Design'
+	end
+
+	def meta_keywords_content
+		'Denver mobile app development, Denver mobile application development, Denver mobile app developer, Denver mobile application developer, Denver mobile app developers, Denver mobile app development company, Denver iphone app development, Denver iOS app development, Denver Android app development, Denver app development, Denver Web Design'
+	end
 end
