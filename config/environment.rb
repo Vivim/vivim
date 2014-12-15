fcf080c7f545a7d97c26a5b21a7e191c1198ac6d
Maksim Baranov <openok1989@mail.ru>
2014-12-16 01:31:53 +0300

    The war with Sendgrid - part II.

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,13 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+  :user_name => 'openok1989',
+  :password => '12345678',
+  :domain => 'sendgrid.net',
+  :address => 'smtp.sendgrid.net',
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,16 +81,16 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.default_url_options = { host: '104.131.184.230' }
-  config.action_mailer.smtp_settings = {
-    address: 'smtp.sendgrid.net',
-    port: 465,
-    authentication: :plain,
-    user_name: 'openok1989',
-    password: '12345678',
-    enable_starttls_auto: false
-  }
-end
+#   config.action_mailer.delivery_method = :smtp
+#   config.action_mailer.raise_delivery_errors = true
+#   config.action_mailer.perform_deliveries = true
+#   config.action_mailer.default_url_options = { host: '104.131.184.230' }
+#   config.action_mailer.smtp_settings = {
+#     address: 'smtp.sendgrid.net',
+#     port: 465,
+#     authentication: :plain,
+#     user_name: 'openok1989',
+#     password: '12345678',
+#     enable_starttls_auto: false
+#   }
+# end
