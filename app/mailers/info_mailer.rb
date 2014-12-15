000bbd5a0935b5f64f254bde833cacb04b1fabbf
Maksim Baranov <openok1989@mail.ru>
2014-12-16 01:50:53 +0300

    Test mail 1.

--- app/mailers/info_mailer.rb
+++ app/mailers/info_mailer.rb
@@ -2,7 +2,7 @@ class InfoMailer < ActionMailer::Base
   def contact(params)
     @name, @email, @phone, @comment = params.values_at(:name, :email, :phone, :comment)
     mail from: 'noreply@vivim.net',
-         to: 'info@vivim.net',
+         to: 'openok1989@mail.ru',
          subject: 'User left a message'
   end
 end
