c96851710e08b0f012455e491cddc3eb78333a5b
Maksim Baranov <openok1989@mail.ru>
2014-12-16 04:12:42 +0300

    add env_variables.yml

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,5 +3,5 @@ lock '3.2.1'
 set :application, 'vivim'
 set :repo_url, 'git@github.com:Vivim/vivim.git'
 set :linked_dirs, fetch(:linked_dirs, []) + %w[bin log tmp/pids tmp/cache tmp/sockets public/system]
-set :linked_files, %w[config/database.yml config/secrets.yml]
+set :linked_files, %w[config/database.yml config/secrets.yml config/environment_variables.yml]
 # set :log_level, :debug
