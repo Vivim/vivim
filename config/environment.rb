c4a054ffdc28a8d47a4c826efaac00385a2eebae
Maksim Baranov <openok1989@mail.ru>
2014-12-20 22:36:44 +0300

    add changes of mailer config.

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,13 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+  :user_name => ENV['SMTP_USER'],
+  :password => ENV['SMTP_PASS'],
+  :domain => 'vivim.net',
+  :address => 'smtp.sendgrid.net',
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
